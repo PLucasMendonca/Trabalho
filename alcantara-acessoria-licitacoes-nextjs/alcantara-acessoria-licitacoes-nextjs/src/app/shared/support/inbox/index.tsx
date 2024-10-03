6eb4837e1de19db00fa1a213cfd8366c4a772ec4
Lucas Mendonca <p.lucas.santos.mendonca@gmail.com>
2024-10-03 16:47:37 -0300

    Novos

--- alcantara-acessoria-licitacoes-nextjs/alcantara-acessoria-licitacoes-nextjs/src/app/shared/support/inbox/index.tsx
+++
@@ -1,9 +0,0 @@
-import InboxTabs from "@/app/shared/support/inbox/inbox-tabs";
-
-export default function SupportInbox() {
-  return (
-    <div className="@container">
-      <InboxTabs className="hidden @4xl:col-span-8 @4xl:block @[1550px]:col-span-8" />
-    </div>
-  );
-}
